fad915085362818f72a4952bdfa7ac9fb6f7bcdd
Maksim Stepanov <stmmaksim@gmail.com>
2024-09-24 23:20:13 +0300

    Setup UI

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
